da023ec0df8a7a0fc8d4276759040e5ce370fe0d
Tom Barclay <tom@tombarclay.com>
2016-10-27 15:57:53 -0700

    adding code

--- MarkovAstroPapers/core.py
+++ MarkovAstroPapers/core.py
@@ -10,6 +10,9 @@ from __future__ import (absolute_import, division, print_function,
 import markovify
 import pandas
 
+from twython import Twython
+from secrets import *
+
 def printReference(year):
     df = pd.read_json('data/{}.json'.format(year), )
     df.sort_index(inplace=True)
@@ -20,3 +23,10 @@ def printReference(year):
     author = df['lastname'].value_counts()[df['lastname'].value_counts() > 1].sample().index[0]
 
     print('{} et al., {} ({})'.format(author,outtitle,year))
+
+def post_tweet(status,):
+    """Post an animated gif and associated status message to Twitter."""
+    twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    response = twitter.update_status(status=status, )
+    print(response)
+    return twitter, response
